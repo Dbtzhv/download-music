db04553dd0ca0dfb9951e68ba5668225ca9c804d
Deni <denibatyzhev@gmail.com>
2023-05-23 19:14:23 +0500

    docker commit

--- music/settings.py
+++ music/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'music.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+        'PORT': '5432',
+        'NAME': 'hello_django_dev',
+        'USER': 'hello_django',
+        'PASSWORD': 'hello_django',
     }
 }
 
